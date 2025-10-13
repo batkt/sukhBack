4f8074eb7df3e96c5e7c27aa2818aeb31fb2e9d5
bilgeedev <jrrsenpai@gmail.com>
2025-10-13 13:05:47 +0800

    z

--- routes/gereeRoute.js
+++ routes/gereeRoute.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Geree = require("../models/geree");
+const { crud } = require("zevbackv2");
+
+crud(router, "geree", Geree);
+
 module.exports = router;
