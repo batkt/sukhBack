5f4a1e31a656dc9c1ddc0f2e1e9568fb970d0772
bilgeedev <jrrsenpai@gmail.com>
2025-10-13 16:50:20 +0800

    z

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ mongoose.connect(dbUri, {
   useUnifiedTopology: true,
 });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
