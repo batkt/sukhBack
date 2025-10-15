87b141532d04997c884c53f9fb8c1d95137a5d7d
bilgeedev <jrrsenpai@gmail.com>
2025-10-15 14:10:03 +0800

    z

--- routes/ajiltanRoute.js
+++ routes/ajiltanRoute.js
@@ -5,7 +5,12 @@ const NevtreltiinTuukh = require("../models/nevtreltiinTuukh");
 const BackTuukh = require("../models/backTuukh");
 const Baiguullaga = require("../models/baiguullaga");
 const request = require("request");
-const { crudWithFile, crud, UstsanBarimt } = require("zevbackv2");
+const {
+  crudWithFile,
+  crud,
+  UstsanBarimt,
+  tokenShalgakh,
+} = require("zevbackv2");
 const { ajiltanNevtrey } = require("../controller/ajiltan");
 
 crudWithFile(
@@ -19,23 +24,23 @@ crudWithFile(
   UstsanBarimt,
   async (req, res, next) => {
     try {
-      // const { db } = require("zevbackv2");
-      // var ajiltanModel = Ajiltan(db.erunkhiiKholbolt);
-      // if (req.params.id) {
-      //   var ObjectId = require("mongodb").ObjectId;
-      //   var ajiltan = await ajiltanModel.findOne({
-      //     nevtrekhNer: req.body.nevtrekhNer,
-      //     _id: { $ne: ObjectId(req.params.id) },
-      //   });
-      //   if (ajiltan) throw new Error("Нэвтрэх нэр давхардаж байна!");
-      // } else {
-      //   if (req.body.nevtrekhNer) {
-      //     var ajiltan = await ajiltanModel.findOne({
-      //       nevtrekhNer: req.body.nevtrekhNer,
-      //     });
-      //     if (ajiltan) throw new Error("Нэвтрэх нэр давхардаж байна!");
-      //   }
-      // }
+      const { db } = require("zevbackv2");
+      var ajiltanModel = Ajiltan(db.erunkhiiKholbolt);
+      if (req.params.id) {
+        var ObjectId = require("mongodb").ObjectId;
+        var ajiltan = await ajiltanModel.findOne({
+          nevtrekhNer: req.body.nevtrekhNer,
+          _id: { $ne: ObjectId(req.params.id) },
+        });
+        if (ajiltan) throw new Error("Нэвтрэх нэр давхардаж байна!");
+      } else {
+        if (req.body.nevtrekhNer) {
+          var ajiltan = await ajiltanModel.findOne({
+            nevtrekhNer: req.body.nevtrekhNer,
+          });
+          if (ajiltan) throw new Error("Нэвтрэх нэр давхардаж байна!");
+        }
+      }
       next();
     } catch (error) {
       next(error);
