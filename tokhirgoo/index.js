a71837b1ba1eda25982710d92918e844e1698c2e
bilgeedev <jrrsenpai@gmail.com>
2025-10-21 10:53:32 +0800

    z

--- tokhirgoo/index.js
+++ tokhirgoo/index.js
@@ -7,4 +7,9 @@ module.exports = {
   APP_SECRET: process.env.APP_SECRET,
   UNDSEN_IP: process.env.UNDSEN_IP,
   BAAZ: process.env.BAAZ,
-}
+  MSG_SERVER: process.env.MSG_SERVER,
+  TOKI_SERVER: process.env.TOKI_SERVER,
+  TOKI_USERNAME: process.env.TOKI_USERNAME,
+  TOKI_PASSWORD: process.env.TOKI_PASSWORD,
+  TOKI_3RD_PARTY: process.env.TOKI_3RD_PARTY,
+};
