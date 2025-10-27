bb5f48f5b270a9ee0dea657527ec899193061366
bilgeedev <jrrsenpai@gmail.com>
2025-10-27 15:14:50 +0800

    z

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ async function automataarNekhemjlekhUusgekh() {
 
 // Өдөр бүр 10:10 цагт ажиллах cron job
 cron.schedule(
-  "13 15 * * *", // Өдөр бүр 10:10 цагт
+  "16 15 * * *", // Өдөр бүр 10:10 цагт
   function () {
     automataarNekhemjlekhUusgekh();
   },
