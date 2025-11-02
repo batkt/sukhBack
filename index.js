2cd7b3fd75412ade85ab3d284a63331822520cd1
Anzai <55052716+owari-hash@users.noreply.github.com>
2025-11-02 16:47:38 +0800

    z

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ async function automataarNekhemjlekhUusgekh() {
 
 // Өдөр бүр 10:10 цагт ажиллах cron job
 cron.schedule(
-  "40 16 * * *", // Өдөр бүр 10:10 цагт
+  "49 16 * * *", // Өдөр бүр 10:10 цагт
   function () {
     automataarNekhemjlekhUusgekh();
   },
