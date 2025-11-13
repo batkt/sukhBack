1b7053ed936a2d4894e69bbd1cf72539856321ce
bilgeedev <jrrsenpai@gmail.com>
2025-11-13 14:50:21 +0800

    z

--- routes/tailanRoute.js
+++ routes/tailanRoute.js
@@ -13,27 +13,27 @@ const {
 } = require("../controller/tailan");
 
 // Өр, авлагын тайлан (оршин суугчдийн) - Байр, орц, давхар, тоогоор хайж хэн төлбөрөө төлсөн, хэн төлөөгүйг хянах
-router.all("/tailan/orlogo-avlaga", tokenShalgakh, tailanOrlogoAvlaga);
+crud(router,"/tailan/orlogo-avlaga", tokenShalgakh, tailanOrlogoAvlaga);
 
 // Сарын төлбөр тайлан (сар сараар нэмээд улиралаар шүүж харах боломжтой, хураангуй дэлгэрэнгүй)
-router.all("/tailan/sariin-tulbur", tokenShalgakh, tailanSariinTulbur);
+crud(router,"/tailan/sariin-tulbur", tokenShalgakh, tailanSariinTulbur);
 
 // Нэхэмжлэлийн түүх (Бүх үүссэн нэхэмжлэлийн жагсаалтыг хянах)
-router.all("/tailan/nekhemjlekhiin-tuukh", tokenShalgakh, tailanNekhemjlekhiinTuukh);
+crud(router,"/tailan/nekhemjlekhiin-tuukh", tokenShalgakh, tailanNekhemjlekhiinTuukh);
 
 // Авлагын насжилтийн тайлан (Төлөгдөөгүй төлбөрийн насжилтыг тодорхойлох)
-router.all("/tailan/avlagiin-nasjilt", tokenShalgakh, tailanAvlagiinNasjilt);
+crud(router,"/tailan/avlagiin-nasjilt", tokenShalgakh, tailanAvlagiinNasjilt);
 
 // Гүйцэтгэлийн тайлан (Сарын төлөвлөгөөт орлого vs бодит орлого г.м ба Зардлын төсөв vs бодит зардал г.м)
-router.all("/tailan/guitsegtgel", tokenShalgakh, tailanGuitsegtgel);
+crud(router,"/tailan/guitsegtgel", tokenShalgakh, tailanGuitsegtgel);
 
 // Төлөгдөөгүй удсан авлага 2+ сар
-router.all("/tailan/udsan-avlaga", tokenShalgakh, tailanUdsanAvlaga);
+crud(router,"/tailan/udsan-avlaga", tokenShalgakh, tailanUdsanAvlaga);
 
 // Цуцлагдсан гэрээний авлага
-router.all("/tailan/tsutslasan-gereenii-avlaga", tokenShalgakh, tailanTsutslasanGereeniiAvlaga);
+crud(router,"/tailan/tsutslasan-gereenii-avlaga", tokenShalgakh, tailanTsutslasanGereeniiAvlaga);
 
 // Тайланг excel/pdf-р татаж авах боломж
-router.all("/tailan/export", tokenShalgakh, tailanExport);
+crud(router,"/tailan/export", tokenShalgakh, tailanExport);
 
 module.exports = router;
