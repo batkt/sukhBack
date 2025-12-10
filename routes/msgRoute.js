f7de5a4d9134526f1bc4ceabecd5f9b7bb2322ea
Zola <zzolbaya@gmail.com>
2025-12-10 10:40:09 +0800

    medegdel olon hund ilgeeh

--- routes/msgRoute.js
+++ routes/msgRoute.js
@@ -1,23 +1,126 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const FormData = require("form-data");
+const request = require("request");
 const { tokenShalgakh, db } = require("zevbackv2");
 const Baiguullaga = require("../models/baiguullaga");
-const OrshinSuugch = require("../models/orshinSuugch");
-const { sendSms } = require("../services/smsService");
+const MsgTuukh = require("../models/msgTuukh");
+
+function msgIlgeeye(
+  jagsaalt,
+  key,
+  dugaar,
+  khariu,
+  index,
+  next,
+  req,
+  res,
+  kholbolt,
+  baiguullagiinId,
+  barilgiinId
+) {
+  try {
+    const url = encodeURI(
+      `${process.env.MSG_SERVER}/send?key=${key}&from=${dugaar}&to=${jagsaalt[index].to}&text=${jagsaalt[index].text}`
+    );
+
+    request(url, { json: true }, async (err1, _, body) => {
+      if (err1) {
+        next(err1);
+      } else {
+        const MsgTuukhModel = MsgTuukh(kholbolt);
+        await MsgTuukhModel.create({
+          baiguullagiinId: baiguullagiinId,
+          barilgiinId: barilgiinId,
+          dugaar: [jagsaalt[index].to],
+          gereeniiId: jagsaalt[index].gereeniiId,
+          msg: jagsaalt[index].text,
+          msgIlgeekhKey: key,
+          msgIlgeekhDugaar: dugaar,
+        });
+
+        if (jagsaalt.length > index + 1) {
+          khariu.push(body[0]);
+          msgIlgeeye(
+            jagsaalt,
+            key,
+            dugaar,
+            khariu,
+            index + 1,
+            next,
+            req,
+            res,
+            kholbolt,
+            baiguullagiinId,
+            barilgiinId
+          );
+        } else {
+          khariu.push(body[0]);
+          res.send(khariu);
+        }
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function msgIlgeeyeUnitel(
+  jagsaalt,
+  key,
+  dugaar,
+  khariu,
+  _index,
+  next,
+  _req,
+  res,
+  kholbolt,
+  baiguullagiinId,
+  barilgiinId
+) {
+  try {
+    for (const data of jagsaalt) {
+      const form = new FormData();
+      form.append("token_id", key);
+      form.append("extension_number", "11");
+      form.append("sms_number", dugaar);
+      form.append("to", data.to.toString());
+      form.append("body", data.text.toString());
+
+      const resp = await axios.post(
+        "https://pbxuc.unitel.mn/hodupbx_api/v1.4/sendSms",
+        form,
+        { headers: form.getHeaders() }
+      );
+
+      if (resp?.data?.status === "SUCCESS") {
+        const MsgTuukhModel = MsgTuukh(kholbolt);
+        await MsgTuukhModel.create({
+          baiguullagiinId: baiguullagiinId,
+          barilgiinId: barilgiinId,
+          dugaar: [data.to],
+          gereeniiId: data.gereeniiId,
+          msg: data.text,
+          msgIlgeekhKey: key,
+          msgIlgeekhDugaar: dugaar,
+        });
+
+        khariu.push(resp.data);
+      }
+    }
+    res.send(khariu?.length > 0 ? [khariu[0]] : []);
+  } catch (err) {
+    next(err);
+  }
+}
 
 router.route("/msgIlgeeye").post(tokenShalgakh, async (req, res, next) => {
   try {
-    const {
-      orshinSuugchId,
-      baiguullagiinId,
-      barilgiinId,
-      medeelel,
-      turul,
-    } = req.body;
+    const { baiguullagiinId, barilgiinId, msgnuud } = req.body;
 
     if (!baiguullagiinId) {
       return res.status(400).json({
-        success: false,
         message: "baiguullagiinId is required",
       });
     }
@@ -27,23 +130,8 @@ router.route("/msgIlgeeye").post(tokenShalgakh, async (req, res, next) => {
     );
 
     if (!kholbolt) {
-      return res.status(404).json({
-        success: false,
-        message: "Холболтын мэдээлэл олдсонгүй",
-      });
-    }
-
-    if (!orshinSuugchId) {
       return res.status(400).json({
-        success: false,
-        message: "orshinSuugchId is required",
-      });
-    }
-
-    if (!medeelel || !medeelel.title || !medeelel.body) {
-      return res.status(400).json({
-        success: false,
-        message: "medeelel (title and body) is required",
+        message: "Тохиргоо хийгдээгүй байна!",
       });
     }
 
@@ -55,52 +143,47 @@ router.route("/msgIlgeeye").post(tokenShalgakh, async (req, res, next) => {
 
     if (!msgIlgeekhKey || !msgIlgeekhDugaar) {
       return res.status(400).json({
-        success: false,
-        message: "Мэссэж илгээх тохиргоо хийгдээгүй байна!",
+        message: "Мсж илгээх тохиргоо хийгдээгүй байна!",
       });
     }
 
-    const orshinSuugchIds = Array.isArray(orshinSuugchId)
-      ? orshinSuugchId
-      : [orshinSuugchId];
-
-    const OrshinSuugchModel = OrshinSuugch(kholbolt);
-    const residents = await OrshinSuugchModel.find({
-      _id: { $in: orshinSuugchIds },
-    }).select("utas");
-
-    const smsMessages = residents
-      .filter((resident) => resident.utas && resident.utas.trim() !== "")
-      .map((resident) => ({
-        to: resident.utas,
-        text: `${medeelel.title}\n${medeelel.body}`,
-        baiguullagiinId: baiguullagiinId,
-        barilgiinId: barilgiinId,
-        turul: turul || "medegdel",
-      }));
-
-    if (smsMessages.length === 0) {
+    if (!msgnuud || msgnuud.length === 0) {
       return res.status(400).json({
-        success: false,
-        message: "Утасны дугаартай оршин суугч олдсонгүй",
+        message: "msgnuud is required",
       });
     }
 
-    const smsResults = await sendSms(
-      smsMessages,
-      msgIlgeekhKey,
-      msgIlgeekhDugaar,
-      kholbolt
-    );
+    const khariu = [];
 
-    return res.json({
-      success: true,
-      message: "Мэссэж амжилттай илгээгдлээ",
-      results: smsResults,
-      total: smsMessages.length,
-      sent: smsResults.filter((r) => r.status === "SUCCESS").length,
-      failed: smsResults.filter((r) => r.status === "FAILED").length,
-    });
+    if (msgIlgeekhKey === "g25dFjT1y1upZLYR") {
+      await msgIlgeeyeUnitel(
+        msgnuud,
+        msgIlgeekhKey,
+        msgIlgeekhDugaar,
+        khariu,
+        0,
+        next,
+        req,
+        res,
+        kholbolt,
+        baiguullagiinId,
+        barilgiinId
+      );
+    } else {
+      msgIlgeeye(
+        msgnuud,
+        msgIlgeekhKey,
+        msgIlgeekhDugaar,
+        khariu,
+        0,
+        next,
+        req,
+        res,
+        kholbolt,
+        baiguullagiinId,
+        barilgiinId
+      );
+    }
   } catch (error) {
     next(error);
   }
