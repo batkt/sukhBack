a9f8d1047f1dd3cbd7cf88dfd7764bf085c2fc4f
bilgeedev <jrrsenpai@gmail.com>
2026-01-27 14:14:12 +0800

    z

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ async function automataarNekhemjlekhUusgekh() {
 
 //nehemjleh ilgeeh tsag
 const cronJob = cron.schedule(
-  "013 14 * * *",
+  "16 14 * * *",
   function () {
     const now = new Date();
     console.log(
