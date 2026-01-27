780f73571175004c296e60b78762aa2564de23a9
bilgeedev <jrrsenpai@gmail.com>
2026-01-27 11:16:17 +0800

    z

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ async function automataarNekhemjlekhUusgekh() {
 
 // Schedule cron job to run daily at 16:18 (4:18 PM) Mongolia time
 const cronJob = cron.schedule(
-  "05 11 * * *",
+  "18 11 * * *",
   function () {
     const now = new Date();
     console.log(
