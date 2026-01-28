4cec67a45c7b34026c16e36ddda678448fab56a4
bilgeedev <jrrsenpai@gmail.com>
2026-01-28 15:45:00 +0800

    z

--- routes/orshinSuugchRoute.js
+++ routes/orshinSuugchRoute.js
@@ -331,8 +331,8 @@ router
   .route("/khayagaarBaiguullagaAvya/:duureg/:horoo/:soh")
   .get(khayagaarBaiguullagaAvya);
 
-router.post("/dugaarBatalgaajuulya", dugaarBatalgaajuulya); 
-router.post("/dugaarBatalgaajuulakh", dugaarBatalgaajuulakh); 
+// router.post("/dugaarBatalgaajuulya", dugaarBatalgaajuulya); 
+// router.post("/dugaarBatalgaajuulakh", dugaarBatalgaajuulakh); 
 
 router.post("/orshinSuugchBatalgaajuulya", orshinSuugchBatalgaajuulya);
 router.post("/utasBatalgaajuulakhLogin", utasBatalgaajuulakhLogin);
